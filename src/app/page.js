d6b182cf3967f95ba49f70b8c252e4115951006a
Rohit Bhisikar <rohitbhisikar@icloud.com>
2024-03-23 19:35:49 +0530

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,71 @@
+'use client'
+import Image from 'next/image'
+import styles from './page.module.css'
+import { useRef, useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+export default function Home() {
+
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const slider = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
+
+  useEffect( () => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.25,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: e => direction = e.direction * -1
+      },
+      x: "-500px",
+    })
+    requestAnimationFrame(animate);
+  }, [])
+
+  const animate = () => {
+    if(xPercent < -100){
+      xPercent = 0;
+    }
+    else if(xPercent > 0){
+      xPercent = -100;
+    }
+    gsap.set(firstText.current, {xPercent: xPercent})
+    gsap.set(secondText.current, {xPercent: xPercent})
+    requestAnimationFrame(animate);
+    xPercent += 0.1 * direction;
+  }
+
+  return (
+    <main className={styles.main}>
+      <Image 
+        src="/images/_DSC4162.JPG"
+        fill={true}
+        alt="background"
+      />
+      <div className={styles.sliderContainer}>
+        <div ref={slider} className={styles.slider}>
+          <p ref={firstText}>Creative Writer -</p>
+          <p ref={secondText}>Creative Writer -</p>
+        </div>
+      </div>
+      <Overlay/>
+    </main>
+  )
+}
+
+
+function Overlay() {
+  return (
+    <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
+      <a href="/" style={{ position: 'absolute', bottom: 40, left: 40, fontSize: '13px' }}> <br /> </a>
+      <a href="/" style={{ position: 'absolute', top: 40, left: 40, fontSize: '15px'}}> wasted potential —</a>
+      <div style={{ position: 'absolute', top: 40, right: 40, fontSize: '15px' }}> Surabhi Kulkarni</div>
+    </div>
+  );
+}
